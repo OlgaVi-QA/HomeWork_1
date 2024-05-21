99926df6996f84c6a6a46392cd1bf2fa219a90d7
OlgaVi-QA <olgavitest@gmail.com>
2024-05-21 11:23:34 +0300

    Первый блин комом

---
+++ homework1/src/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+    public static void main(String[] args) {
+        static int count = 0;
+        String name;
+        Kotik1 kotik1 = new Kotik1(name);
+
+
+        class Kotik1 {
+            Kotik1 kotik1 = new Kotik1();
+
+        }
+    }
+}
+
